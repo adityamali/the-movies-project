9c23cf7c715345e64468531b7c7a87b53e74ba1a
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2023-11-03 18:48:59 +0530

    fetch data exports

---
+++ src/fetchData.js
@@ -0,0 +1,42 @@
+export function getMovies(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results);
+
+        main.innerHTML = '';
+
+        for (let i = 0; i < data.results.length; i++) {
+            const movieEl = document.createElement('div');
+            movieEl.classList.add('movie');
+            movieEl.innerHTML = `
+        <img src="${imgURL + data.results[i].backdrop_path}" alt="${data.results[i].original_title}">
+        <div class="movie-info">
+            <h5>${data.results[i].original_title}</h5>
+            <span id = "ratebg" class="${getColor(data.results[i].vote_average)}">${data.results[i].vote_average}</span>
+        </div>
+        <div class="overview">
+            ${data.results[i].overview}
+        </div>
+        <div class="add">
+            +
+        </div>
+        `
+            main.appendChild(movieEl);
+        }
+
+    })
+
+}
+
+function getColor(rating) {
+    if (rating >= 7.5) {
+        return 'green';
+    }
+    else if (rating >= 4.5) {
+        return 'orange';
+    }
+    else {
+        return 'red';
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,65 +1,16 @@
 import _ from 'lodash';
 
 import { searchURL, imgURL, apiURL } from './tmdbassets';
-
-// const apiKey = 'api_key=85cb3ce779175fccfcf2729ae6cfe0fb';
-// const baseURL = 'https://api.themoviedb.org/3/';
-// const apiURL = baseURL + 'discover/movie?sort_by=popularity.desc&' + apiKey;
-// const imgURL = 'https://image.tmdb.org/t/p/w500';
-// const searchURL = baseURL + 'search/movie?' + apiKey;
+import { getMovies } from './fetchData';
 
 const main = document.getElementById('main');
 
 const form = document.getElementById('form');
 const search = document.getElementById('search')
 
+
 getMovies(apiURL);
 
-function getMovies(url) {
-
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results);
-
-        main.innerHTML = '';
-
-        for (let i = 0; i < data.results.length; i++) {
-            const movieEl = document.createElement('div');
-            movieEl.classList.add('movie');
-            movieEl.innerHTML = `
-        <img src="${imgURL + data.results[i].backdrop_path}" alt="${data.results[i].original_title}">
-        <div class="movie-info">
-            <h5>${data.results[i].original_title}</h5>
-            <span id = "ratebg" class="${getColor(data.results[i].vote_average)}">${data.results[i].vote_average}</span>
-        </div>
-        <div class="overview">
-            ${data.results[i].overview}
-        </div>
-        <div class="add">
-            +
-        </div>
-        `
-            main.appendChild(movieEl);
-        }
-
-    })
-
-}
-
-function showMovies(data) {
-
-}
-
-function getColor(rating) {
-    if (rating >= 7.5) {
-        return 'green';
-    }
-    else if (rating >= 4.5) {
-        return 'orange';
-    }
-    else {
-        return 'red';
-    }
-}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
