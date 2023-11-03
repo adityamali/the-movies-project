2aaf217ccc0c50a0e6d8944fd0db4719a1ebcaa9
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2023-11-03 16:21:31 +0530

    added webpack for bundling

---
+++ src/tmdbassets.js
@@ -0,0 +1,5 @@
+export const apiKey = 'api_key=85cb3ce779175fccfcf2729ae6cfe0fb';
+export const baseURL = 'https://api.themoviedb.org/3/';
+export const apiURL = baseURL + 'discover/movie?sort_by=popularity.desc&' + apiKey;
+export const imgURL = 'https://image.tmdb.org/t/p/w500';
+export const searchURL = baseURL + 'search/movie?' + apiKey;
