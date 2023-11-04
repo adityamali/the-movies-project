0f341c3d437a37a758df117d38cd238e12a61a1e
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2023-11-04 15:02:08 +0530

    Fixed a bug caused by Dotenv improper config

--- src/tmdbassets.js
+++ src/tmdbassets.js
@@ -1,6 +1,4 @@
-TMDBapi = process.env.TMDBapi;
-
-export const apiKey = 'api_key=' + TMDBapi;
+export const apiKey = 'api_key=' + process.env.TMDBapi;
 export const baseURL = 'https://api.themoviedb.org/3/';
 export const apiURL = baseURL + 'discover/movie?sort_by=popularity.desc&' + apiKey;
 export const imgURL = 'https://image.tmdb.org/t/p/w500';
