24ccd805b4a368e3d2cd2d1ffa64de2b1825b4ee
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2023-11-04 17:03:39 +0530

    api update 2

--- src/tmdbassets.js
+++ src/tmdbassets.js
@@ -1,4 +1,4 @@
-export const apiKey = 'api_key=' + process.env.NEXT_PUBLIC_TMDBapi;
+export const apiKey = 'api_key=' + process.env.TMDBapi;
 export const baseURL = 'https://api.themoviedb.org/3/';
 export const apiURL = baseURL + 'discover/movie?sort_by=popularity.desc&' + apiKey;
 export const imgURL = 'https://image.tmdb.org/t/p/w500';
